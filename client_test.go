b3ccbd10da838e0021fdfe5a322c43efb77f1114
Boris Gern <b.gern@tinkoff.ru>
2018-10-04 17:55:36 +0300

    100% test coverage

--- client_test.go
+++ client_test.go
@@ -1,11 +1,32 @@
 package main
 
 import (
-	//"fmt"
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"os"
+	"strings"
 	"testing"
+	"time"
 )
+
+type root struct {
+	XMLName xml.Name `xml:"root"`
+	Rows    []row    `xml:"row"`
+}
+
+type row struct {
+	XMLName   xml.Name `xml:"row"`
+	ID        int      `xml:"id"`
+	Age       int      `xml:"age"`
+	Gender    string   `xml:"gender"`
+	LastName  string   `xml:"last_name"`
+	FirstName string   `xml:"first_name"`
+	About     string   `xml:"about"`
+}
 
 func TestInvalidAccesToken(t *testing.T) {
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -84,8 +105,78 @@ func TestReqParams(t *testing.T) {
 		t.Errorf("Shouldn't be able to proceed request with limit < 0\n")
 	}
 	response, err = client.FindUsers(SearchRequest{Limit: 26, Offset: -1})
-	//fmt.Println(response, err)
 	if response != nil || err.Error() != "offset must be > 0" {
 		t.Errorf("Shouldn't be able to proceed request with offset < 0\n")
+	}
+}
+
+func queryXML(query string, limit string) []User {
+	xmlFile, err := os.Open("dataset.xml")
+	if err != nil {
+		fmt.Printf("Can't open file: %v\n", err)
+	}
+	defer xmlFile.Close()
+	byteValue, _ := ioutil.ReadAll(xmlFile)
+	var usersXML root
+	err = xml.Unmarshal(byteValue, &usersXML)
+	if err != nil {
+		fmt.Printf("Can't parse XML: %v\n", err)
+	}
+	users := make([]User, 0)
+	for _, user := range usersXML.Rows {
+		name := user.FirstName + " " + user.LastName
+		if strings.Contains(name, query) || strings.Contains(user.About, query) {
+			var u User
+			u.Id = user.ID
+			u.About = user.About
+			u.Age = user.Age
+			u.Gender = user.Gender
+			u.Name = name
+			users = append(users, u)
+		}
+	}
+	return users
+}
+
+func TestResult(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		query := r.URL.Query().Get("query")
+		limit := r.URL.Query().Get("limit")
+		users := queryXML(query, limit)
+		b, err := json.Marshal(users)
+		if err != nil {
+			fmt.Printf("Can't marshal users: %v\n", err)
+		}
+		w.Write(b)
+	}))
+	client := &SearchClient{URL: server.URL}
+	response, err := client.FindUsers(SearchRequest{Query: "Wolf", Limit: 17})
+	if response == nil || err != nil {
+		t.Errorf("Shouldn't be able to receive result\n")
+	}
+	response, err = client.FindUsers(SearchRequest{Query: "enim", Limit: 17})
+	if response.NextPage != true || err != nil {
+		t.Errorf("Should show next page true\n")
+	}
+}
+
+func TestResponseUnknownError(t *testing.T) {
+	httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
+	client := &SearchClient{}
+	response, err := client.FindUsers(SearchRequest{})
+	if response != nil || err.Error() != `unknown error Get ?limit=1&offset=0&order_by=0&order_field=&query=: unsupported protocol scheme ""` {
+		t.Errorf("Shouldn't be able to receive result\n")
+	}
+}
+
+func TestResponseError(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(3 * time.Second)
+	}))
+	client := &SearchClient{URL: server.URL}
+	response, err := client.FindUsers(SearchRequest{})
+	if response != nil || err.Error() != "timeout for limit=1&offset=0&order_by=0&order_field=&query=" {
+		t.Errorf("Should be timeout\n")
 	}
 }
